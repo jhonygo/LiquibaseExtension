25eb7bc95971c327a3affb79808b43f6fd91dd17
sguclu <sguclu@finaxys.com>
2016-06-15 00:56:28 +0200

    add parameters and options

--- LiquibaseExtension/tasks/TaskLiquibaseExecute/liquibaseWrapper.ps1
+++ LiquibaseExtension/tasks/TaskLiquibaseExecute/liquibaseWrapper.ps1
@@ -1,21 +1,24 @@
 [CmdletBinding()]
 param(
-  [string]$changeLogFile,
-  [string]$username,
-  [string]$password,
-  [string]$url,
-  [string]$command,
-  [string]$arguments
+  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $driver,
+  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $changeLogFile,
+  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $username,
+  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $password,
+  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $url,
+  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $command,   
+  [string]$parameters,
+  [string]$buildNumber = $env:BUILD_BUILDNUMBER,
+  [string]$runtime = "liquibase.bat"
 )
 
-# For more information on the VSTS Task SDK:
 try {
+
     # Set the working directory.
     #$cwd = Get-VstsInput -Name cwd -Require
     #Assert-VstsPath -LiteralPath $cwd -PathType Container
     #Write-Verbose "Setting working directory to '$cwd'."
     #Set-Location $cwd
+	Write "Executing '$runtime' --driver='$driver' --url='$url' --username='$username' --password='XXXXXXXX' '$command' '$parameters' " 
+	$A = Start-Process -FilePath '$runtime' --driver='$driver' --url='$url' --username='$username' --password='$password' '$command' '$parameters' -Wait -passthru;$a.ExitCode
 
-    # Output the message to the log.
-    Write-Host (Get-VstsInput -Name $username)
 } finally {}
