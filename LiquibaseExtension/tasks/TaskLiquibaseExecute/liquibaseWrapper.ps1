7fea1fae2fc61ae7c93b95463c5d24e631cc9c07
jhonygo <sedat.guclu@free.fr>
2016-06-16 22:55:39 +0200

    set up appropriate options

--- LiquibaseExtension/tasks/TaskLiquibaseExecute/liquibaseWrapper.ps1
+++ LiquibaseExtension/tasks/TaskLiquibaseExecute/liquibaseWrapper.ps1
@@ -1,30 +1,35 @@
 [CmdletBinding()]
 param(
   # VARIABLES TAKEN FROM THE VSTS EXTENSION
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $changeLogFile,
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $dbms,
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $username,
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $password,
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $hostname,
+  [string] [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] $changeLogFile,
+  [string] [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] $dbms,
+  [string] [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] $username,
+  [string] [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] $password,
+  [string] [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] $hostname,
   [string] $port,
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $dbname,  
-  [string]$databaseOptions,
-  [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $command,   
-  [string]$parameters,
-  [string]$buildNumber = $env:BUILD_BUILDNUMBER,
+  [string] $dbname,  
+  [string] $databaseOptions,
+  [string] [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] $command,   
+  [string] $parameters,
+  [string] $buildNumber = $env:BUILD_BUILDNUMBER,
   # CONSTANTS
-  [string]$runtime = "liquibase.bat",
-  [string]$driver = "lib/jtds-1.3.1.jar"
+  [string] $runtime = "liquibase.bat",
+  [string] $driver = "net.sourceforge.jtds.jdbc.Driver"
 )
 
 try {
-    # Set the working directory.
-    #$cwd = Get-VstsInput -Name $=cwd -Require
-    #Assert-VstsPath -LiteralPath $cwd -PathType Container
-    #Write-Verbose "Setting working directory to '$cwd'."
-    #Set-Location $cwd
-    [string] $url="TODO CONSTRUCT URL DBMS/HOSTNAME(/PORT)(/DATABASEOPTIONS)"
-    
+    # Set JDBC URL with all optional parameters if any
+    [string] $url = "jdbc:jtds:$(dbms)://$(hostname)"
+    If ($port -ne 0) { 
+      $url = "$(url):$(port)"
+    }
+    If (-Not $dbname -Match "") { 
+      $url = "$(url)/$(dbname)"
+    }
+    If (-Not $databaseOptions -Match "") { 
+      $url = "$(url);$(databaseOptions)"
+    }
+
     Write "Executing $runtime --driver=$driver --url=$url --username=$username --password=XXXXXXXX $command $parameters" 
     $A = Start-Process -FilePath "$runtime" --driver="$driver" --url="$url" --username="$username" --password="$password" "$command" "$parameters" -Wait -passthru;$a.ExitCode
 
